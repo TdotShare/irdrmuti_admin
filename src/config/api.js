14314198fda0b1147e8166b36acbab624d52b86b
TdotShare <52649379+TdotShare@users.noreply.github.com>
2020-04-21 00:33:49 +0700

    .

--- src/config/api.js
+++ src/config/api.js
@@ -1,5 +1,5 @@
-//const ROOT_CORS = "https://cors-anywhere.herokuapp.com/";
+const ROOT_CORS = "https://cors-anywhere.herokuapp.com/";
 //const ROOT_API = `${ROOT_CORS}http://www.servicestdot.com/api/irdrmuti_api/public`
-//const ROOT_API = `${ROOT_CORS}http://www.ird.rmuti.ac.th/2020/word/api`
-const ROOT_API = ``
+const ROOT_API = `${ROOT_CORS}http://www.ird.rmuti.ac.th/2020/word/api`
+//const ROOT_API = ``
 export default ROOT_API
